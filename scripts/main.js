9115b4eae26e28145e94a49b6c056d9dbf2b6e83
Victoria Nicholson <saintvictoria@gmail.com>
2014-11-14 08:50:22 -0500

    working early in the morning

--- scripts/main.js
+++ scripts/main.js
@@ -60,6 +60,7 @@
 	})
 
 }());
+
 (function (){
 
 
@@ -156,7 +157,7 @@ App.Views.homeView = Parse.View.extend({
 		},
 
 
-
+		
 	});
 
 }());
@@ -270,15 +271,12 @@ App.Views.singlePost = Parse.View.extend({
       tagName: 'ul',
       className: 'blogList',
 
-      template: _.template($('#allBlogPosts').html()),
+      template: $('#allBlogPosts').html(),
 
       initialize: function (options) {
         $('#welcomePage').empty();
         this.render();
         $('#blogList').html(this.$el);
-
-             
-
 
         this.options = options;
 
@@ -286,19 +284,20 @@ App.Views.singlePost = Parse.View.extend({
         this.collection.off();
         this.collection.on('sync', this.blogQuery, this);
 
-
-
-
         this.blogQuery();
+      },
+
+      render: function(){
+        this.$el.html(this.template)
       },
 
       blogQuery: function () {
 
         var self = this;
 
-      var blog_writer = new Parse.Query(App.Models.blogPost);
-      blog_writer.equalTo('user', App.user);
-      blog_writer.find({
+      var blog_author = new Parse.Query(App.Models.blogPost);
+      blog_author.equalTo('user', App.user);
+      blog_author.find({
         success: function (results) {
           self.collection = results;
           self.render();
